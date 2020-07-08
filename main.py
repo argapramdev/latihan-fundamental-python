89c4c0fe59bb77dd99cf202e6b4938c6ec8cb7b0
Arga Pramudita <argapram@gmail.com>
2020-07-08 22:11:06 +0700

    belajar sampe percabangan if else

---
+++ main.py
@@ -0,0 +1,13 @@
+# latihan eksekusi python
+# sequential = eksekusi berurutan
+print('hello world')
+print('by arga')
+print('tanggal 7 juli 2020')
+print('-'*10)
+
+#percabangan dengan eksekusi terpilih
+ingin_cepat = False
+if ingin_cepat:
+    print('jalan lurus aja ya!')
+else:
+    print('belok kanan saja!')
